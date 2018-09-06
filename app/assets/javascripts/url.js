d5e15ae5bbf5b1b303e1e923a8f126276c55b579
IshinShibata <shibataishin@shibata-Mac.local>
2018-09-06 22:46:15 +0900

    jqueryでajaxメソッドを使用してajaxを実行するようにする

--- app/assets/javascripts/url.js
+++ app/assets/javascripts/url.js
@@ -1,34 +1,34 @@
-window.onload = function() {
+$(document).ready(function() {
 
-    var urls = location.href
+var urls = location.href
+        $.ajax({
+            url: "/urls.json",
+            type: "POST",
+            data: {content: urls},
+            dataType: "json" })
+            .done(function(data) {
+              console.log(data);
+            })
+            .fail(function() {
+              console.log("失敗");
+            })
+});
 
-    const action = '/urls.json';
-    const method = 'POST';
+// window.onload = function() {
 
-    urls.setAttribute(action);
-    urls.setAttribute(method); // POSTリクエストもしくはGETリクエストを書く。
-    urls.style.display = 'none'; // 画面に表示しないことを指定する
+//     var urls = location.href
+
+//     const action = '/urls.json';
+//     const method = 'POST';
+
+//     urls.setAttribute(action);
+//     urls.setAttribute(method); // POSTリクエストもしくはGETリクエストを書く。
+//     urls.style.display = 'none'; // 画面に表示しないことを指定する
 
 
-}
+// }
 
 
     // urls.method = 'POST';
     // urls.action = '/urls.json';
 
-
-// $(document).ready(function() {
-
-// var urls = location.href
-//         $.ajax({
-//             url: "/urls.json",
-//             type: "POST",
-//             data: {content: urls},
-//             dataType: "json" })
-//             .done(function(data) {
-//               console.log(data);
-//             })
-//             .fail(function() {
-//               console.log("失敗");
-//             })
-// });
