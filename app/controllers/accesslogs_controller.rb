0cc45a9fc03b9f45701622bc8d6d95df9952b58d
IshinShibata <shibataishin@shibata-Mac.local>
2018-09-10 02:41:19 +0900

    タグ発行からユーザから指定があった日時のpv数を出力
    index action
    @access_log_array@access_log_urlsをデフォルトで0にしてindex.html.erbで実行されてもnilclas
    sにならないようにする。
    
    ブラウザから送られてきたsettingsキーにネストされたtokenキー,dateキー,urlキーをparamsからそれぞれ変数に格納。
    
    paramsで送られてきたtokenとidetifierカラムの値が一致するレコードをfind_byでDBから引っ張り、@tokenへ格納。
    
    @tokenとリレーションを組んでいるaccess_logsからレコードを引っ張る。
    
    0~23時の１日の測定結果をを出力したい為、タイムズクラスで一時間の範囲begin_timeとend_timeにタイムオブジェクトを格納し、ブラ
    ウザから指定があった日の中で一致するaccesslogtablesのレコードを数を出力する。
    ブラウザではurlを指定できるので、urlが指定された場合と指定されない場合の条件分岐を記載する。
    
    セレクトボックスでurlを選択してもらう為、表記する文字とキーをセットにした配列を出力。
    
    token_create action
    Tokenインスタンスを生成し、uuidで生成された乱数字をidentifierカラム代入し、
    DBに保存
    保存されたtokenをデータ属性としてユーザーに貼り付けてもらうscriptタグに配置し@tagに格納後、ブラウザでタグ発行のリクエストがあった
    場合、出力する。
    
    create action
    scriptタグを貼り付けたhtmlがアクセスされる度に送られてきたparamsのscriptタグにデータ属性として貼り付けたtokenをキーと
    一致するtokenレコード出力し、tokenとリレーションを組んだaccess_logsレコードをparamsで送られてきたurlを引数にしてD
    Bに保存する。

--- app/controllers/accesslogs_controller.rb
+++ app/controllers/accesslogs_controller.rb
@@ -1,19 +1,51 @@
 class AccesslogsController < ApplicationController
-
+  protect_from_forgery except: :create
 
   def index
-    token = Token.find(1)
-    date = params[:date]
-    # binding.pry
-    @accesslogs = token.access_logs
-
     @access_log_array = []
+    @access_logs_urls = []
+    return unless params.has_key?(:settings)
+    token = params[:settings][:token]
+    date = params[:settings][:date]
+    url = params[:settings][:url]
+    @token = Token.find_by(identifier: token)
+    @accesslogs = @token.access_logs
+
     24.times do |hour|
       begin_time = Time.zone.parse("#{date} #{hour}:00")
       end_time = Time.zone.parse("#{date} #{hour}:59")
-      @access_log_array << @accesslogs.where(created_at: begin_time..end_time).count
+
+      if url.present?
+        @access_log_array << @accesslogs.where(created_at: begin_time..end_time, url: url).count
+      else
+        @access_log_array << @accesslogs.where(created_at: begin_time..end_time).count
+      end
+    end
+    @accesslogs.each do |access_log|
+      @access_logs_urls << [access_log.url, access_log.url]
     end
   end
 
+  def token_create
+    @access_log_array = []
+    @access_logs_urls = []
 
+    token = Token.new
+    random = SecureRandom.uuid
+    token.identifier = random
+    token.save
+    @tag = "<script id=\"data\" data-token=\"#{token.identifier}\" async src=\"http://localhost:3000/pv_analytics.js\"></script>"
+    render :index
+  end
+
+  def create
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "POST"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    # binding.pry
+    token = Token.find_by(identifier: params[:token])
+    token.access_logs.create(url: params[:url])
+    {}
+  end
 end
+
