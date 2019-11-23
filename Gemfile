30eb672befaad43892a66240c5f23a0e5c455b1a
柴田維新 <kobukobudesu.dekaihou@gmail.com>
2019-11-23 09:59:23 +0900

    gemfile database.yml編集

--- Gemfile
+++ Gemfile
@@ -50,6 +50,11 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
+
+group :production do
+ gem 'mysql2'
+end
+
 gem 'pry-rails'
 gem 'devise'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- public/pv_analytics.js
+++ public/pv_analytics.js
@@ -4,9 +4,9 @@ xhr.open("POST", 'http://localhost:3000/accesslogs/create', true);
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
 xhr.onreadystatechange = function() {
-    if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+  if(this.readyState == XMLHttpRequest.DONE && this.status == 200) {
 
-    }
+  }
 }
 var el = document.querySelector('#data');
 var url = location.href
